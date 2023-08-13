545c21d20a6d1cb4dd6cafac43c5d6078314c5bb
Delecroix <d-alexis@hotmail.fr>
2023-08-13 18:41:14 +0200

    Mise en place du serveur et connexion BDD

---
+++ server/routes/user.route.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const router = express.Router();
+const userController = require('../controllers/user.controller');
+
+// Get All Pokémon
+router.get('/allPokemon', userController.getAllPokemon);
+
+// Get All Pokémon LVL 1
+router.get('/allPokemonLvl', userController.getAllPokemonLvl)
+
+module.exports = router;
