d9b45dc1b0d66e9cbe1bd4e90f51e7fbf60386b4
Nhours <alapideseb@hotmail.fr>
2023-08-24 15:00:18 +0200

    redirection sur la page Accueil

--- client/presentation/js/app.js
+++ client/presentation/js/app.js
@@ -44,3 +44,12 @@ window.onclick = function(event) {
         closeModalSeb();
     }
 }
+
+// Fonction pour revenir à la page d'accueil
+function returnToHomePage() {
+    window.location.href = "../Accueil/index.html"; // Remplacez par le chemin correct
+}
+
+// Ajouter un écouteur d'événement au clic sur l'élément avec la classe "header-icons"
+const headerIconsElement = document.querySelector(".header-icons");
+headerIconsElement.addEventListener("click", returnToHomePage);
