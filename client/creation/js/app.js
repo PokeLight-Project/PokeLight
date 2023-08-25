540c8bbc437cdb11e494e9f3cf16edef55f01c18
Delecroix <d-alexis@hotmail.fr>
2023-08-25 11:57:28 +0200

    mise en forme de css

--- client/creation/js/app.js
+++ client/creation/js/app.js
@@ -1,127 +1,4 @@
-// "use strict"
-
-// document.addEventListener("DOMContentLoaded", function () {
-//     const myAudio = document.getElementById("myAudio");
-//     const playAudio = document.getElementById("volume");
-//     let Playing = false;
-
-//     playAudio.addEventListener("click", () => {
-//         if (!Playing) {
-//             myAudio.play();
-//             Playing = true;
-//             playAudio.classList.remove("fa-solid");
-//             playAudio.classList.remove("fa-volume-xmark");
-//             playAudio.classList.add("fa-solid");
-//             playAudio.classList.add("fa-volume-high");
-//         } else {
-//             myAudio.pause();
-//             Playing = false;
-//             playAudio.classList.remove("fa-solid");
-//             playAudio.classList.remove("fa-volume-high");
-//             playAudio.classList.add("fa-solid");
-//             playAudio.classList.add("fa-volume-xmark");
-//         }
-//     });
-
-//     async function httpGet(url) {
-//         const query = await fetch(url)
-
-//         const response = await query.json();
-//         return response;
-//     }
-
-//     function getAllPokemonLvl1() {
-//         let pokemonLvl1 = document.getElementById("pokemonLvl1");
-
-//         const response = httpGet("http://localhost:8000/allPokemonLvl")
-
-//         console.log(response);
-
-//         response.then((data) => {
-
-//             console.log(data);
-//             data.forEach(element => {
-//                 let color = "";
-//                 if (element.type_pokemon == "plante") {
-//                     color = "background:rgba(0, 255, 26, 0.5);"
-//                 } else if (element.type_pokemon == "feu") {
-//                     color = "background:rgba(255, 0, 0, 0.5);"
-//                 } else if (element.type_pokemon == "eau") {
-//                     color = "background:rgba(0, 133, 255, 0.5);"
-//                 } else {
-//                     color = "background:rgba(255, 138, 0, 0.5);"
-//                 }
-//                 pokemonLvl1.innerHTML += `
-//                                          <div class="card" style="${color}" id="pokemon-${element.id_pokemon}">
-//                                             <i id="${element.id_pokemon}" class="fa-sharp fa-solid fa-question interrogation"></i>
-//                                             <img src="${element.image_url_pokemon}" alt="Photo de ${element.name_pokemon}">
-//                                         </div>`;
-
-//             });
-//             // Récupération les points d'interrogation pour afficher les stats
-//             let openStat = document.querySelectorAll(".interrogation")
-
-//             console.log(openStat);
-
-//             openStat.forEach(element => {
-//                 element.addEventListener("click", () => {
-//                     const pokemonId = element.id;
-//                     console.log(pokemonId);
-//                     const response = httpGet(`http://localhost:8000/onePokemon/${pokemonId}`);
-//                     console.log(response);
-
-//                     // Mettre à jour la carte du Pokémon avec les nouvelles informations
-//                     response.then((data) => {
-//                         console.log(data);
-//                         const pokemonCard = document.getElementById(`pokemon-${pokemonId}`);
-
-//                         let img = ""
-//                         if (data[0].type_pokemon == "plante") {
-//                             img = "../Imgs/pokémons/typePlante.png"
-//                         } else if (data[0].type_pokemon == "feu") {
-//                             img = "../Imgs/pokémons/typeFeu.png"
-//                         } else if (data[0].type_pokemon == "eau") {
-//                             img = "../Imgs/pokémons/typeEau.png"
-//                         } else {
-//                             img = "../Imgs/pokémons/typeFeu.png"
-//                         }
-
-//                         // Remplacez le contenu de la carte avec les nouvelles informations
-//                         pokemonCard.innerHTML = `
-//                             <i id="${element.id}" class="fa-solid fa-xmark cross"></i>
-//                                 <div class="card2">
-
-//                                 <img id="typePokemon" src="${img}" alt="Photo du type ${data[0].type_pokemon}"> 
-//                                 <p> PV : ${data[0].pv_pokemon} </p>
-//                                 <p> PV : ${data[0].pa_pokemon} </p>
-//                                 </div>
-//                             `;
-//                         let closeStat = document.querySelector(".cross")
-
-//                         console.log(closeStat);
-//                         closeStat.addEventListener("click", () => {
-//                             console.log("coucou");
-//                             const pokemonId = element.id;
-//                             console.log(pokemonId);
-
-//                             pokemonCard.innerHTML = `
-//                             <i id="${pokemonId}" class="fa-sharp fa-solid fa-question interrogation"></i>
-//                                 <img src="${data[0].image_url_pokemon}" alt=""> 
-//                             `;
-//                         })
-//                     });
-//                 });
-
-//             })
-//         }
-//         )
-       
-        
-//     }
-//     getAllPokemonLvl1();
-    
-// })
-
+"use strict"
 
 document.addEventListener("DOMContentLoaded", async function () {
     // Récupération des éléments audio et du bouton de lecture
@@ -156,7 +33,7 @@ document.addEventListener("DOMContentLoaded", async function () {
     }
 
     // Fonction pour remplir la liste des Pokémon
-    async function populatePokemonList() {
+    async function listPokemonLvl1() {
         let pokemonLvl1 = document.getElementById("pokemonLvl1");
 
         // Attendre la réponse de la requête HTTP pour obtenir la liste des Pokémon
@@ -241,5 +118,30 @@ document.addEventListener("DOMContentLoaded", async function () {
     });
 
     // Charger la liste des Pokémon lors du chargement de la page
-    populatePokemonList();
+    listPokemonLvl1();
+
+
+    async function listPokedex() {
+
+        const listPokedex = document.getElementById("pokedex")
+
+        const response = await httpGet("http://localhost:8000/allPokedex")
+
+        console.log(response);
+        
+        response.forEach(data => {
+            listPokedex.innerHTML += `
+                                    <div class="listPokedex">
+                                            <p> ${data.username_user}</p>
+                                            <img src="${data.image_url_pokemon}" alt="Photo du pokemon d'${data.username_user}">
+                                            <i class="fa-solid fa-trash-can delete"></i>
+                                            <div class="hp"></div>
+                                            <p> LVL : ${data.level}</p>
+                                     </div>
+                                    `
+        });
+        
+    }
+
+    listPokedex();
 });
