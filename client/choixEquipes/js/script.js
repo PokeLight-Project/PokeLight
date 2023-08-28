787033bbe9894853696bc964cd0cb4c4d8734e41
Charlie-Chap <charlesjserin@gmail.com>
2023-08-28 12:00:06 +0200

    Btn combat

--- client/choixEquipes/js/script.js
+++ client/choixEquipes/js/script.js
@@ -170,4 +170,33 @@ document.addEventListener("DOMContentLoaded", () => {
     }
 
     getAllPokedex();
+
+
+
+
+
+
+
+    const combatBtn = document.getElementById("combat_btn");
+
+    function updateCombatButton() {
+        const hasRedPokemons = dragdropRed.querySelectorAll(".card").length > 0;
+        const hasFloraPokemons = dragdropFlora.querySelectorAll(".card").length > 0;
+
+        if (hasRedPokemons || hasFloraPokemons) {
+            combatBtn.style.backgroundColor = "#E70E0E";
+            combatBtn.disabled = false;
+        } else {
+            combatBtn.style.backgroundColor = "";
+            combatBtn.disabled = true;
+        }
+    }
+
+    combatBtn.addEventListener("click", () => {
+        // Ajoutez ici le code pour rediriger vers la page "combat"
+        window.location.href = "../combat/combat.html";
+    });
+
+    // Ajoutez cet appel pour mettre à jour le bouton au chargement de la page
+    updateCombatButton();
 });
