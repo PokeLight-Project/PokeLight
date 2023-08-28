a28633cdb4d21038e8220b90fe2d9d18ea957ec6
Delecroix <d-alexis@hotmail.fr>
2023-08-28 16:26:32 +0200

    mise en place du combat

--- client/combat/js/combat.js
+++ client/combat/js/combat.js
@@ -12,6 +12,28 @@ document.addEventListener("DOMContentLoaded", async function () {
 
     const myAudio = document.getElementById("myAudio")
 
+    const playAudio = document.getElementById("volume");
+    let Playing = false;
+
+    // Gestionnaire de clic pour le bouton de lecture audio
+    playAudio.addEventListener("click", () => {
+        if (!Playing) {
+            myAudio.play();
+            Playing = true;
+            playAudio.classList.remove("fa-solid");
+            playAudio.classList.remove("fa-volume-xmark");
+            playAudio.classList.add("fa-solid");
+            playAudio.classList.add("fa-volume-high");
+        } else {
+            myAudio.pause();
+            Playing = false;
+            playAudio.classList.remove("fa-solid");
+            playAudio.classList.remove("fa-volume-high");
+            playAudio.classList.add("fa-solid");
+            playAudio.classList.add("fa-volume-xmark");
+        }
+    });
+
 
     async function GetbackGround() {
         const response = await httpGet("http://localhost:8000/getMap")
