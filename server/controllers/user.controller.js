c24ba839161a2f99ef2dba04b66bc7b58f0b3bb0
Delecroix <d-alexis@hotmail.fr>
2023-08-30 09:10:28 +0200

    w3c

--- server/controllers/user.controller.js
+++ server/controllers/user.controller.js
@@ -144,40 +144,6 @@ const getAllPokedex = (req, res) => {
     })
 }
 
-// Créer un nouveau message
-const createMessage = async (req, res) => {
-    const { content_message } = req.body;
-
-    if (!content_message) {
-        return res.status(400).json({
-            error: 'Contenu du message manquant'
-        });
-    }
-
-    try {
-        const createMessageQuery = 'INSERT INTO `message` (`content_message`) VALUES (?)';
-        await conn.query(createMessageQuery, [content_message]);
-
-        res.status(200).json({ message: 'Message enregistré avec succès' });
-    } catch (error) {
-        console.error('Erreur lors de la requête', error);
-        res.status(500).json({ error: 'Erreur lors de la requête' });
-    }
-};
-
-// Récupérer tous les messages
-const getAllMessages = (req, res) => {
-    const query = 'SELECT * FROM message';
-    conn.query(query, (err, result) => {
-        if (err) {
-            console.error("Erreur lors de la récupération des données :", err);
-            res.status(500).json({ error: "Erreur lors de la récupération des données" })
-        } else {
-            res.status(200).json(result)
-        }
-    })
-}
-
 // Obtenez les informations sur les utilisateurs dans l'équipe "Red"
 const getTeamRedInfo = async (req, res) => {
         const query = `
@@ -201,6 +167,9 @@ const getTeamRedInfo = async (req, res) => {
        });
 };
 
+// Créer un nouveau message
+const createMessage = async (req, res) => {
+    const { content_message } = req.body;
 
     if (!content_message) {
         return res.status(400).json({
@@ -277,25 +246,6 @@ const getTeamFloraInfo = async (req, res) => {
    });
 };
 
-// DeleteOneUserPokedex
-const deleteOneUserPokedex = (req, res) => {
-    const userId = req.params.id;
-    if (!userId) {
-        return res.status(400).json({
-            error: 'ID de l\'utilisateur manquant dans les paramètres de la route',
-        })
-    }
-    // Construction de la requête SQL pour supprimer l'utilisateur
-    let query = `DELETE FROM pokedex WHERE id_user = ${userId}`
-
-    conn.query(query, (err) => {
-        if (err) {
-            console.error('Erreur lors de la suppression de l\'utilisateur');
-            res.status(500).json({ error: 'Erreur lors de la suppression de l\'utilisateur' });
-        } else {
-            res.status(200).json({ message: 'Utilisateur supprimer' });
-        }
-    });
 
 module.exports = {
     getAllPokemon,
@@ -308,5 +258,6 @@ module.exports = {
     createTeamFlora,
     getBackGround,
     getTeamFloraInfo,
-    getTeamRedInfo
+    getTeamRedInfo,
+ createMessage, getAllMessages, deleteOneUserPokedex
 }
